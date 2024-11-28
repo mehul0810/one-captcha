a51004e8d5c749dc72ac5d4788c2551d94eef538
Mehul Gohil <mehul.gohil@get10up.com>
2024-11-28 16:43:34 +0530

    add operational plugin file

---
+++ src/Plugin.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * OneCaptcha | Plugin File.
+ *
+ * @package WordPress
+ * @subpackage OneCaptcha
+ * @since 1.0.0
+ */
+
+namespace MG\OneCaptcha;
+
+use MG\OneCaptcha\Includes as Includes;
+use MG\OneCaptcha\Admin as Admin;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Loads and registers plugin functionality through WordPress hooks.
+ *
+ * @since 1.0.0
+ */
+final class Plugin {
+	/**
+	 * Registers functionality with WordPress hooks.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function register() {
+		// Handle plugin activation and deactivation.
+		register_activation_hook( ONECAPTCHA_PLUGIN_FILE, [ $this, 'activate' ] );
+		register_deactivation_hook( ONECAPTCHA_PLUGIN_FILE, [ $this, 'deactivate' ] );
+
+		// Register services used throughout the plugin.
+		add_action( 'plugins_loaded', [ $this, 'register_services' ] );
+
+		// Load text domain.
+		add_action( 'init', [ $this, 'load_plugin_textdomain' ] );
+	}
+
+	/**
+	 * Registers the individual services of the plugin.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function register_services() {
+		// Load Admin Files.
+		if ( is_admin() ) {
+			new Admin\Actions();
+			new Admin\Filters();
+		}
+
+		// Load Frontend Files.
+		new Includes\Actions();
+		new Includes\Filters();
+	}
+
+	/**
+	 * Loads the plugin's translated strings.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain(
+			'onecaptcha',
+			false,
+			dirname( plugin_basename( ONECAPTCHA_PLUGIN_FILE ) ) . '/languages/'
+		);
+	}
+
+	/**
+	 * Handles activation procedures during installation and updates.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @param bool $network_wide Optional. Whether the plugin is being enabled on
+	 *                           all network sites or a single site. Default false.
+	 *
+	 * @return void
+	 */
+	public function activate( $network_wide = false ) {}
+
+	/**
+	 * Handles deactivation procedures.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function deactivate() {}
+}
