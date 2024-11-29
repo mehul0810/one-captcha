ec57f88d9f0c3bd12e024a30dffa78ca7b9be7ca
Mehul Gohil <mehul.gohil@get10up.com>
2024-11-29 17:52:42 +0530

    load conditional api for captcha service selected

--- src/Includes/Actions.php
+++ src/Includes/Actions.php
@@ -22,5 +22,16 @@ class Actions {
      * @access public
      */
     public function __construct() {
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_assets' ] );
     }
+
+	public function register_assets() {
+		$active_service = Helpers::get_active_service();
+
+		// Load API script based on active service.
+		if ( 'cloudflare_turnstile' === $active_service ) {
+			// Load Cloudflare Turnstile API script.
+			wp_enqueue_script( 'onecaptcha-clouflare-turnstile-api', 'https://challenges.cloudflare.com/turnstile/v0/api.js', [], ONECAPTCHA_VERSION, true );
+		}
+	}
 }
