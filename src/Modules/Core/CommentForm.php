4fc01cdbc8a3f857021d1d7ded66a211d09cbf08
Mehul Gohil <mehul.gohil@get10up.com>
2024-12-02 23:50:50 +0530

    implement comment form logic

--- src/Modules/Core/CommentForm.php
+++ src/Modules/Core/CommentForm.php
@@ -10,6 +10,7 @@
 namespace MG\OneCaptcha\Modules\Core;
 
 use MG\OneCaptcha\Modules\ModuleInit;
+use MG\OneCaptcha\API;
 
 // Bailout, if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -30,6 +31,8 @@ class CommentForm extends ModuleInit {
 	 * @access public
 	 */
 	public function __construct() {
+		parent::__construct();
+
 		add_action( 'comment_form_after_fields', [ $this, 'add_to_comment_form' ] );
 		add_action( 'pre_comment_on_post', [ $this, 'verify_on_comment_submission' ] );
 	}
@@ -43,8 +46,8 @@ class CommentForm extends ModuleInit {
 	 * @return void
 	 */
 	public function add_to_comment_form() : void {
-		$service     = $this->active_service();
-		$credentials = $this->active_service_credentials();
+		$service     = $this->active_service;
+		$credentials = $this->active_service_credentials;
 		$site_key    = $credentials['site_key'] ?? '';
 		$secret_key  = $credentials['secret_key'] ?? '';
 
@@ -67,22 +70,14 @@ class CommentForm extends ModuleInit {
 	 * @return void
 	 */
 	public function verify_on_comment_submission() : void {
-		$service = $this->active_service();
+		$service = $this->active_service;
 
-		if (
-			'cloudflare_turnstile' === $service &&
-			(
-				(
-					isset( $_POST['cf-turnstile-response'] ) &&
-					empty( $_POST['cf-turnstile-response'] )
-				) ||
-				(
-					! empty( $_POST['cf-turnstile-response'] ) &&
-					false === json_decode( $_POST['cf-turnstile-response'] )['success']
-				)
-			)
-		) {
-			wp_die(__('Captcha verification failed. Please try again.', 'onecaptcha'));
+		if ( 'cloudflare_turnstile' === $service ) {
+			$this->verify_cloudflare_turnstile_response();
+		} else if ( 'google_recaptcha' === $service ) {
+			$this->verify_google_recaptcha_v2_response();
+		} else if ( 'hcaptcha' === $service ) {
+			$this->verify_hcaptcha_response();
 		}
 	}
 
@@ -104,6 +99,32 @@ class CommentForm extends ModuleInit {
 	}
 
 	/**
+	 * Verify Cloudflare Turnstile Response.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function verify_cloudflare_turnstile_response() : void {
+		// Get token from POST request.
+		$token = $_POST['cf-turnstile-response'] ?? '';
+
+		// Bailout, if token is empty.
+		if ( empty( $token ) ) {
+			wp_die( __( 'Captcha verification failed. Please try again.', 'onecaptcha' ) );
+		}
+
+		// Verify Cloudflare Turnstile Captcha.
+		$response = API\CloudflareTurnstile::verify( $token, $this->active_service_credentials['secret_key'] );
+
+		// Bailout, if response is false.
+		if ( ! $response ) {
+			wp_die( __( 'Captcha verification failed. Please try again.', 'onecaptcha' ) );
+		}
+	}
+
+	/**
 	 * Render Google reCAPTCHA v2 Checkbox HTML.
 	 *
 	 * @param string $site_key Site Key.
@@ -118,6 +139,32 @@ class CommentForm extends ModuleInit {
 			'<div class="g-recaptcha" data-sitekey="%1$s"></div>',
 			$site_key
 		);
+	}
+
+	/**
+	 * Verify Google reCaptcha v2 Response.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function verify_google_recaptcha_v2_response() : void {
+		// Get token from POST request.
+		$captcha_response = $_POST['g-recaptcha-response'] ?? '';
+
+		// Bailout, if response is empty.
+		if ( empty( $captcha_response ) ) {
+			wp_die( __( 'Captcha verification failed. Please try again.', 'onecaptcha' ) );
+		}
+
+		// Verify Google ReCaptcha.
+		$response = API\GoogleReCaptcha::verify( $captcha_response, $this->active_service_credentials['secret_key'] );
+
+		// Bailout, if response is false.
+		if ( ! $response ) {
+			wp_die( __( 'Captcha verification failed. Please try again.', 'onecaptcha' ) );
+		}
 	}
 
 	/**
@@ -136,5 +183,31 @@ class CommentForm extends ModuleInit {
 			$site_key,
 			apply_filters( 'onecaptcha_hcaptcha_theme', 'light' )
 		);
+	}
+
+	/**
+	 * Verify hCaptcha Response.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function verify_hcaptcha_response() : void {
+		// Get token from POST request.
+		$captcha_response = $_POST['h-captcha-response'] ?? '';
+
+		// Bailout, if response is empty.
+		if ( empty( $captcha_response ) ) {
+			wp_die( __( 'Captcha verification failed. Please try again.', 'onecaptcha' ) );
+		}
+
+		// Verify hCaptcha.
+		$response = API\hCaptcha::verify( $captcha_response, $this->active_service_credentials['secret_key'] );
+
+		// Bailout, if response is false.
+		if ( ! $response ) {
+			wp_die( __( 'Captcha verification failed. Please try again.', 'onecaptcha' ) );
+		}
 	}
 }
