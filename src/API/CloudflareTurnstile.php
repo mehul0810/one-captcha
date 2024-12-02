c2135860c212545c741495fc077be502c942703e
Mehul Gohil <mehul.gohil@get10up.com>
2024-12-02 23:44:02 +0530

    update logic to verify cf captcha

--- src/API/CloudflareTurnstile.php
+++ src/API/CloudflareTurnstile.php
@@ -27,19 +27,27 @@ class CloudflareTurnstile {
 	 *
 	 * @return void
 	 */
-	public static function verify( $token ) {
+	public static function verify( $token, $key ) {
 		$response = wp_remote_post('https://challenges.cloudflare.com/turnstile/v0/siteverify', [
             'body' => [
-                'secret' => self::SECRET_KEY,
+                'secret'   => $key,
                 'response' => $token,
             ],
         ]);
 
-        if (is_wp_error($response)) {
+		// Bailout, if response is an error.
+        if ( is_wp_error( $response ) ) {
             return false;
         }
 
-        $response_body = json_decode(wp_remote_retrieve_body($response), true);
-        return $response_body['success'] ?? false;
+        $response_body = json_decode( wp_remote_retrieve_body( $response ), true );
+
+		// Bailout, if response body is empty.
+		if ( empty( $response_body ) ) {
+			return false;
+		}
+
+		// Return response status.
+		return $response_body['success'] ?? false;
 	}
 }
