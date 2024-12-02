abe299cce43e9aa403e70a2cbd17733ff3f31507
Mehul Gohil <mehul.gohil@get10up.com>
2024-12-02 20:33:42 +0530

    add scripts for google recaptcha and hcaptcha

--- src/Includes/Actions.php
+++ src/Includes/Actions.php
@@ -27,6 +27,14 @@ class Actions {
 		add_action( 'wp_enqueue_scripts', [ $this, 'register_assets' ] );
     }
 
+	/**
+	 * Register Assets.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
 	public function register_assets() {
 		$active_service = Helpers::get_active_service();
 
@@ -34,6 +42,12 @@ class Actions {
 		if ( 'cloudflare_turnstile' === $active_service ) {
 			// Load Cloudflare Turnstile API script.
 			wp_enqueue_script( 'onecaptcha-clouflare-turnstile-api', 'https://challenges.cloudflare.com/turnstile/v0/api.js', [], ONECAPTCHA_VERSION, false );
+		} else if ( 'google_recaptcha' === $active_service ) {
+			// Load Google reCAPTCHA API v3 script.
+			wp_enqueue_script( 'onecaptcha-google-recaptcha-api', 'https://www.google.com/recaptcha/api.js', [], ONECAPTCHA_VERSION, false );
+		} else if ( 'hcaptcha' === $active_service ) {
+			// Load hCaptcha API script.
+			wp_enqueue_script( 'onecaptcha-hcaptcha-api', 'https://js.hcaptcha.com/1/api.js', [], ONECAPTCHA_VERSION, false );
 		}
 	}
 }
