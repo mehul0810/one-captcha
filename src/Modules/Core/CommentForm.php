2b80feada771d3191086e6bdafa29e182b211609
Mehul Gohil <mehul.gohil@get10up.com>
2024-12-02 20:52:32 +0530

    improve comment form based on module init

--- src/Modules/Core/CommentForm.php
+++ src/Modules/Core/CommentForm.php
@@ -9,7 +9,7 @@
 
 namespace MG\OneCaptcha\Modules\Core;
 
-use MG\OneCaptcha\Helpers;
+use MG\OneCaptcha\Modules\ModuleInit;
 
 // Bailout, if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @since 1.0.0
  */
-class CommentForm {
+class CommentForm extends ModuleInit {
 
 	/**
 	 * Constructor.
@@ -30,8 +30,8 @@ class CommentForm {
 	 * @access public
 	 */
 	public function __construct() {
-		add_action( 'comment_form_after_fields', [ $this, 'add_turnstile_to_comment_form' ] );
-		add_action( 'pre_comment_on_post', [ $this, 'verify_turnstile_on_comment_submission' ] );
+		add_action( 'comment_form_after_fields', [ $this, 'add_to_comment_form' ] );
+		add_action( 'pre_comment_on_post', [ $this, 'verify_on_comment_submission' ] );
 	}
 
 	/**
@@ -42,11 +42,19 @@ class CommentForm {
 	 *
 	 * @return void
 	 */
-	public function add_turnstile_to_comment_form() : void {
-		$credentials = Helpers::get_active_service_credentials();
+	public function add_to_comment_form() : void {
+		$service     = $this->active_service();
+		$credentials = $this->active_service_credentials();
 		$site_key    = $credentials['site_key'] ?? '';
 		$secret_key  = $credentials['secret_key'] ?? '';
 
+		if ( 'cloudflare_turnstile' === $service ) {
+			$this->render_cloudflare_turnstile_html( $site_key );
+		} else if ( 'google_recaptcha' === $service ) {
+			$this->render_google_recaptcha_v2_checkbox_html( $site_key );
+		} else if ( 'hcaptcha' === $service ) {
+			$this->render_hcaptcha_html( $site_key );
+		}
 		echo '<div class="cf-turnstile" data-sitekey="' . esc_attr( $site_key) . '"></div>';
 	}
 
@@ -71,5 +79,57 @@ class CommentForm {
 		) {
 			wp_die(__('Captcha verification failed. Please try again.', 'onecaptcha'));
 		}
+	}
+
+	/**
+	 * Render Cloudflare Turnstile HTML.
+	 *
+	 * @param string $site_key Site Key.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function render_cloudflare_turnstile_html( $site_key ) : void {
+		echo sprintf(
+			'<div class="cf-turnstile" data-sitekey="%1$s"></div>',
+			$site_key
+		);
+	}
+
+	/**
+	 * Render Google reCAPTCHA v2 Checkbox HTML.
+	 *
+	 * @param string $site_key Site Key.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function render_google_recaptcha_v2_checkbox_html( $site_key ) : void {
+		echo sprintf(
+			'<div class="g-recaptcha" data-sitekey="%1$s"></div>',
+			$site_key
+		);
+	}
+
+	/**
+	 * Render hCaptcha HTML.
+	 *
+	 * @param string $site_key Site Key.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function render_hcaptcha_html( $site_key ) : void {
+		echo sprintf(
+			'<div class="h-captcha" data-sitekey="%1$s" data-theme="%2$s" data-error-callback="onError"></div>',
+			$site_key,
+			apply_filters( 'onecaptcha_hcaptcha_theme', 'light' )
+		);
 	}
 }
